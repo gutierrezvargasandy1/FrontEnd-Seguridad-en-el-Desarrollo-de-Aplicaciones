5b196fe653b05ce8f81c7f167af35de2e191aebe
gutierrezvargasandy1 <gutierrezvargasandy1@gmail.com>
2026-04-12 20:46:59 -0600

    Cambios por hacer

--- front-end/src/app/auth/profile/profile.ts
+++ front-end/src/app/auth/profile/profile.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService, User, LoginError } from '../../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -13,10 +14,35 @@ export class Profile implements OnInit {
   serverError = '';
   fieldErrors: { [key: string]: string } = {}; // Por consistencia, aunque no se use aquí
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.loadProfile();
+  }
+
+  deleteUser(): void {
+    if (confirm('Estas Seguro de eliminar tu perfil Esta accion no se puede Desechar'))
+    {
+      this.loading=true;
+      this.serverError='';
+
+      this.authService.logout().subscribe(
+        {
+          next: () => {
+            this.user = null;
+            this.loading = false;
+            alert('Perfil eliminado Exitosamente')
+            this.router.navigate(['/login']);
+          },
+
+          error: (error: LoginError) => {
+            this.serverError = error.userMessage || 'Error al eliminar el perfil.';
+            this.loading = false;}
+        }
+      )
+
+    }
+
   }
 
   loadProfile(): void {
